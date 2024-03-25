3bd5fc8cff4d12cc658e840e591f2189814d0f6a
Miroslav <mirino.bella@gmail.com>
2024-03-25 20:58:28 +0100

    initial commit

---
+++ src/test/java/com/example/hospitalrecords/HospitalRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
