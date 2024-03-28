c35030d3c730e580819f2c4c56ae999ebf55cf22
Miroslav <mirino.bella@gmail.com>
2024-03-28 11:16:42 +0100

    update

--- src/main/java/com/example/hospitalrecords/controller/HospitalController.java
+++ src/main/java/com/example/hospitalrecords/controller/HospitalController.java
@@ -1,9 +1,13 @@
 package com.example.hospitalrecords.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HospitalController {
 
-
+    @GetMapping("/hospital")
+    public String welcome(){
+        return "Welcome in da Hospital";
+    }
 }
